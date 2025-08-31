62f5ad2ea101f10d897db8d9676351157fd40848
unknown <abhixxxx.007@gmail.com>
2025-08-31 12:28:07 +0530

    added math utility

---
+++ git_pro.java
@@ -0,0 +1,6 @@
+Public class GitPro{
+    public static void main (String[] args){
+        system.out.println("Hello world!");
+        system.out.println("sum" + MathUtils.add(a: 2,b : 3));
+    }
+}
