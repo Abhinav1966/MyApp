d0f619f69455fc7bbd6569608f908267ad7e88ea
unknown <abhixxxx.007@gmail.com>
2025-09-01 10:37:06 +0530

    multiply feature has been added

--- MathUtils.java
+++ MathUtils.java
@@ -5,4 +5,7 @@ public class MathUtils{
     public static int substract(int a, int b){
         return a - b; 
     }
+     public static int multiply(int a, int b){
+        return a * b;
+    }
 }
