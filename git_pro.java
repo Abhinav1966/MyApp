b711439e5d400ad2d0bac9b8d5a55e3725cf9558
unknown <abhixxxx.007@gmail.com>
2025-09-01 09:55:43 +0530

    called utility f in main

--- git_pro.java
+++ git_pro.java
@@ -1,6 +1,6 @@
 public class git_pro{
     public static void main (String[] args){
         System.out.println("Hello world!");
-        System.out.println("sum" + MathUtils.add(a:2, b:3));
+        System.out.println("sum" + MathUtils.add(a:5, b:10));
     }
 }
