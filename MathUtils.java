1ba9b7cc70c5ea4831f314edd0af053672698ca7
Abhinav1966 <abhixxxx.007@gmail.com>
2025-09-04 17:50:39 +0530

    fixed the bug

--- MathUtils.java
+++ MathUtils.java
@@ -1,10 +1,7 @@
 public class MathUtils{
     public static int add(int a, int b){
-<<<<<<< HEAD
+
         return a + b +1; //bug fixed in add.
-=======
-        return a + b -1; //different change
->>>>>>> main
     }
     public static int substract(int a, int b){
         return a - b; 
